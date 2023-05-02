3143d9b75848483c934a1b6b03eb7c8d901741e0
Milywayz <jtjenks@gmail.com>
2023-05-01 21:55:18 -0500

    added a delete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const dbData = require("./db/db.json")
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,41 +16,55 @@ app.get('/', (req, res) =>
 );
 
 // GET request for reviews
-app.get('/api/reviews', (req, res) => {
-  res.status(200).json(reviews);
+app.get('/notes', (req, res) => {
+  res.json(dbData)
+  // res.status(200).json(dbData);
 });
 
-// POST request to add a review
-// NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+app.post('/notes', (req, res) => {
 
-  // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
+  //pull out data from req.body
+  //push to the db array (importing from db)
+  // write to file (db.json)
+  res.status(200).json();
+});
 
-  // If all the required properties are present
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
-    };
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting review');
-  }
+app.delete('/notes', (req, res) => {
+  res.json(dbData)
+  // res.status(200).json(dbData);
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+// // POST request to add a review
+// // NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
+// app.post('/api/reviews', (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to add a review`);
+
+//   // Destructuring assignment for the items in req.body
+//   const { product, review, username } = req.body;
+
+//   // If all the required properties are present
+//   if (product && review && username) {
+//     // Variable for the object we will save
+//     const newReview = {
+//       product,
+//       review,
+//       username,
+//       review_id: uuid(),
+//     };
+
+//     const response = {
+//       status: 'success',
+//       body: newReview,
+//     };
+
+//     console.log(response);
+//     res.status(201).json(response);
+//   } else {
+//     res.status(500).json('Error in posting review');
+//   }
+// });
