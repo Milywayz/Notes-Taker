c9f523a5f88dd43fb744e93663e5f1b9a21b1949
Milywayz <jtjenks@gmail.com>
2023-05-02 22:58:01 -0500

    added an id to each note

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const dbData = require("./db/db.json");
 const { writeFile } = require('fs');
-const dbArray = []
 
 const PORT = process.env.PORT || 3001;
 
@@ -28,12 +27,18 @@ app.get('/api/notes', (req, res) => {
 });
 // Post request for notes
 app.post('/api/notes', (req, res) => {
+
   
-  const noteData = req.body
+  const noteData = req.body 
 
-  dbArray.push(noteData)
+  const noteDataId = Math.random().toString(36);
 
-  const pushedNote = JSON.stringify(dbArray)
+// Add the ID to the noteData object
+  noteData.id = noteDataId;
+
+  dbData.push(noteData)
+
+  const pushedNote = JSON.stringify(dbData)
 
   writeFile(path.join(__dirname, './db/db.json'), pushedNote , (err) =>{
     if (err){
@@ -49,40 +54,9 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes', (req, res) => {
   res.json(dbData)
   // res.status(200).json(dbData);
-  // reviews = reviews.filer(review => review.review_id != req.params.id)
+  reviews = reviews.filer(review => review.review_id != req.params.id)
 });
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-// POST request to add a review
-// NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-
-  // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
-
-  // If all the required properties are present
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
-    };
-
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting review');
-  }
-});
